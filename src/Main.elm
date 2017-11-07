8d88d7fb6fa322353eca87f242220f8c6b4d2401
Malcolm Cumming <bemjogar@gmail.com>
2017-11-07 10:28:03 -0600

    audio tags added to view

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,9 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, on)
+import Random
+import Time exposing (Time, millisecond)
 
 
 -- import Html.Events exposing (..)
@@ -11,26 +13,42 @@ import Html.Events exposing (onClick)
 
 type alias Model =
     { test : String
+    , gameActive : Bool
+    , allowInput : Bool
     , strictMode : Bool
-    , sequence : List Sound
+    , gameSounds : List Sound
+    , sequence : List Int
+    , userSequence : List Int
+    , nextSoundAt : Maybe Time
     }
 
 
 initModel : Model
 initModel =
-    Model "This is working" False []
+    { test = "This is working"
+    , gameActive = False
+    , allowInput = False
+    , strictMode = False
+    , gameSounds = gameSounds
+    , sequence = []
+    , userSequence = []
+    , nextSoundAt = Nothing
+    }
 
 
 type alias Sound =
-    { id : String
-    , soundUrl : String
-    }
-
-
-type alias Quad =
     { id : Int
-    , sound : Sound
+    , url : String
     }
+
+
+gameSounds : List Sound
+gameSounds =
+    [ { id = 1, url = "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3" }
+    , { id = 2, url = "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3" }
+    , { id = 3, url = "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3" }
+    , { id = 4, url = "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3" }
+    ]
 
 
 init : ( Model, Cmd msg )
@@ -44,23 +62,71 @@ init =
 
 type Msg
     = None
-    | StartGame
+    | NewGame
     | ToggleStrict Bool
+    | Tick Time
+    | SelectSound
+    | StaggerSound
+    | PlaySound Int
+    | AddToSequence Int
 
 
-update : Msg -> Model -> ( Model, Cmd msg )
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         None ->
             ( model, Cmd.none )
 
-        StartGame ->
-            ( model, Cmd.none )
+        NewGame ->
+            let
+                sequenceCount =
+                    List.length model.sequence
+
+                newModel =
+                    if sequenceCount > 0 then
+                        { model | sequence = [], userSequence = [], allowInput = False }
+                    else
+                        { model | gameActive = True }
+            in
+                ( newModel, Cmd.none )
 
         ToggleStrict isStrict ->
-            ( {model | strictMode = (not isStrict)}, Cmd.none )
+            ( { model | strictMode = (not isStrict) }, Cmd.none )
 
+        Tick timeNow ->
+            ( model, Cmd.none )
+            -- time model timeNow
 
+        SelectSound ->
+            ( model, Random.generate AddToSequence (Random.int 1 4) )
+
+        AddToSequence id ->
+            ( { model | sequence = id :: model.sequence }, Cmd.none )
+
+        StaggerSound ->
+            ( model, Random.generate PlaySound (Random.int 1 4) )
+        
+        PlaySound multiplier ->
+            playSound model multiplier
+
+startSequence =
+    Cmd.none
+
+time : Model -> Time -> Model
+time model timeNow =
+ case model.nextSoundAt of
+    Nothing ->
+        model
+    Just playAt ->
+        if timeNow <= playAt then
+            model
+        else
+            model
+        
+
+playSound model multiplier =
+    -- if model.
+ ( model , Cmd.none )
 
 -- VIEW
 
@@ -75,6 +141,7 @@ view model =
             , text "by "
             , a [ href "https://www.linkedin.com/in/malcolmcumming/" ] [ text "Malcolm Cumming" ]
             ]
+        , sounds model.gameSounds
         ]
 
 
@@ -112,28 +179,41 @@ controls model =
 
                 Nothing ->
                     "press start"
-        
     in
         div [ class "controls" ]
             [ div [ class "step-count" ] [ text stepCount, span [] [ text stepUnit ] ]
             , label []
                 [ text "start"
-                , button [ type_ "button", name "start", onClick StartGame ] []
+                , button [ type_ "button", name "start", onClick NewGame ] []
                 ]
-            , label [ classList [("strict", model.strictMode)] ]
+            , label []
                 [ text "strict"
-                , button [ type_ "button", name "strict", onClick (ToggleStrict model.strictMode) ] []
+                , button
+                    [ type_ "button"
+                    , name "strict"
+                    , classList [ ( "strict", model.strictMode ) ]
+                    , onClick (ToggleStrict model.strictMode)
+                    ]
+                    []
                 ]
             ]
 
-
+sounds : List Sound -> Html Msg
+sounds soundList =
+    soundList 
+    |> List.map (\sound -> audio [ src sound.url, type_ "audio/mpeg", id ("sound" ++ toString sound.id) ] [])
+    |> div [ id "media" ]
 
 -- SUBSCRIPTIONS
 
 
-subscriptions : Model -> Sub msg
+subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    if model.gameActive && not model.allowInput then
+    Sub.batch
+        [  Time.every (250 * millisecond) Tick
+        ]
+    else Sub.none
 
 
 
